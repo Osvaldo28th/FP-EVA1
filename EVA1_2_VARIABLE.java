063b9336ccf50ee4fbebf24b71ffeeaafad83f80
Osvaldo28th <L24550306@chihuahua2.tecnm.mx>
2024-08-26 20:48:44 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLE.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variable;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA1_2_VARIABLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                                //Tipos de datos
+        System.out.println("Hola Mundo");   //Cadena de texto
+        System.out.println(100);    //Un numero entero
+        System.out.println('C');    //Un caracter
+        System.out.println(true);   //Valores logicos (verdadero/falso)
+        System.out.println(false);
+        System.out.println(1000.52);    //Numeros con fracciones (con decimal)
+        //TIPOS DE DATOS
+        //Numeros Enteros
+        //int 
+        //long 
+        //byte 
+        //short 
+        
+        //Numeros Decimales
+        //double
+        //float
+        
+        //Texto
+        //String
+        
+        //Caracter
+        //char
+        
+        //Logicos
+        //Boolean
+        
+        //¿Como crear una variable?
+        //Declaracion: tipo de dato de la variable.
+        //Que operaciones debe o puede hacer la variable.
+        //Cuanta memoria va a utilizar.
+        int edad;//Declaro una variable de tipo entero llamada "edad"
+         //Una variable no se puede utilizar 2 veces
+        String nombre;
+        String apellidos;
+        double promedio;
+        
+        //Asignacion: Darle valor a la variable
+        //nombre de la variable = valor
+        edad = 18; //estoy asignando el valor 18 dentro de la variable "edad"
+        System.out.println(edad);// println para imprimir el contenido de la variable
+        
+    }
+    
+}
