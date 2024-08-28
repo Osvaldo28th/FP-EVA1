bac0692e555ab9024b80e3229e4e9e4cae41a08a
Osvaldo28th <L24550306@chihuahua2.tecnm.mx>
2024-08-27 22:34:07 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVI.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_automovi;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA1_3_AUTOMOVI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Declaracion de variables
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        //Asignar valores
+        marca = "Ford";
+        modelo = "Mustang";
+        year = 1996;
+        precio = 999000.1;
+        //Imprimir variables
+        System.out.println(marca);  
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+    
+        
+    }
+    
+
---
+++ EVA1_4_TELEVISION.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_television;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA1_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        int pul;
+        double precio;
+       
+        marca = "LG";
+        pul = 65;
+        precio = 8999.99;
+      
+        System.out.println(marca);
+        System.out.println(pul);
+        System.out.println(precio);
+        
+    }
+    
+}
