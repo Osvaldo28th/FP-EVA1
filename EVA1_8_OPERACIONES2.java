d4942b8bbdc9897682fd0f2950c2ed2d1cd9fa59
Osvaldo28th <L24550306@chihuahua2.tecnm.mx>
2024-09-05 15:58:19 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones2;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //OPRRACIONES CON CADENA
+        String nom = "Alexis";
+        String ape= "Encino";
+        String nomCom;
+        //Cadena de texto, la operacion se llama
+        //Concatenacion-unir cadena de textos
+        //El operador de concatenas es el +
+        //Cuando un simblo tiene varias funciones
+        //Se llama sobrecarga (de operadores)
+        
+        nomCom = nom + " " + ape;
+        System.out.println("El nombre es:" + " " + nom);
+        System.out.println("El apellido es:" + " " + ape);
+        System.out.println("El nombre completo es:" + " " + nomCom);
+        
+        
+    }
+    
+}
