087eeef1386cbe98c8faceca7f3820cf8808ce10
Osvaldo28th <L24550306@chihuahua2.tecnm.mx>
2024-09-28 18:04:04 -0600

    Add files via upload

---
+++ EVA1_22_CALIFAS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_22_califas;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA1_22_CALIFAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
